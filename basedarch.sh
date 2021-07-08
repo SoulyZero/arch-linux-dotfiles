b61b49acd552cee9bc2241fbd75f98dd28de59bb
david <studen.david@gmail.com>
2021-07-08 12:01:10 +0200

    yet another fix

--- basedarch.sh
+++ basedarch.sh
@@ -32,15 +32,16 @@ welcome || error
 
 mkdir ~/.config
 
-yay -Sy bpytop dunst i3-gaps kitty neofetch nitrogen neovim picom-tryone-git polybar ranger rofi key-mapper-git nvidia nvidia-utils xorg ttf-all-the-icons ttf-carlito ttf-carlito ttf-dejavu ttf-font-awesome ttf-hack ttf-hanazono ttf-hellvetica ttf-jetbrains-mono ttf-liberation ttf-material-design-icons-git ttf-ms-fonts ttf-win10 ttf-windows || { echo "Please enable yay"; exit 1; }
+yay -Sy bpytop dunst i3-gaps kitty neofetch nitrogen neovim picom-tryone-git polybar ranger rofi key-mapper-git nvidia nvidia-utils xorg ttf-all-the-icons ttf-carlito ttf-carlito ttf-dejavu ttf-font-awesome ttf-hack ttf-hanazono ttf-hellvetica ttf-jetbrains-mono ttf-liberation ttf-material-design-icons-git ttf-ms-fonts ttf-win10 ttf-windows || { exit 1; }
+# some fonts may be missing
+
 
 # cloning
-git clone "https://github.com/dstuden/ArchLinux-dotfiles.git" || { echo "Please install git"; exit 1; };
-mv ArchLinux-dotfiles/.p10k.zsh ~/;
-mv ArchLinux-dotfiles/.vimrc ~/;
-mv ArchLinux-dotfiles/xinitrc ~/;
-mv ArchLinux-dotfiles/.zshrc ~/;
-mv ArchLinux-dotfiles/Wallpapers ~/;
-mv ArchLinux-dotfiles/scripts ~/;
-mv ArchLinux-dotfiles/.config/* ~/.config
-
+git clone "https://github.com/dstuden/ArchLinux-dotfiles.git" || { exit 1; }
+cp -f -r ArchLinux-dotfiles/.p10k.zsh ~/;
+cp -f -r ArchLinux-dotfiles/.vimrc ~/;
+cp -f -r ArchLinux-dotfiles/.xinitrc ~/;
+cp -f -r ArchLinux-dotfiles/.zshrc ~/;
+cp -f -r ArchLinux-dotfiles/Wallpapers ~/;
+cp -f -r ArchLinux-dotfiles/scripts ~/;
+cp -f -r ArchLinux-dotfiles/.config/* ~/.config
